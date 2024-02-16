2dee872f72439312038344f87f77159c35b416eb
jetson <4a9g0026@stust.edu.tw>
2024-02-16 20:01:51 +0800

    update: something

---
+++ code/approaching_image/src/check.py
@@ -0,0 +1,5 @@
+import torch
+print(torch.cuda.is_available())
+
+import torch
+print(torch.cuda.current_device())
---
+++ code/approaching_image/src/model.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import pretrainedmodels
+
+def get_model(pretrained):
+    if pretrained:
+        # model = pretrainedmodels.__dict__['alexnet'](pretrained='imagenet')
+        model = pretrainedmodels.__dict__['resnet18'](pretrained='imagenet')
+    else: 
+        # model = pretrainedmodels.__dict__['alexnet'](pretrained=None)
+        model = pretrainedmodels.__dict__['resnet18'](pretrained='imagenet')
+
+    # print the model here to know whats going on.
+    '''
+    model.last_linear = nn.Sequential(
+        nn.BatchNorm1d(4096),
+        nn.Dropout(p=0.25),
+        nn.Linear(in_features=4096, out_features=2048),
+        nn.ReLU(),
+        nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1),
+        nn.Dropout(p=0.5),
+        nn.Linear(in_features=2048, out_features=1)
+    )
+    '''
+    model.last_linear = nn.Sequential(
+        nn.BatchNorm1d(512),
+        nn.Dropout(p=0.25),
+        nn.Linear(in_features=512, out_features=2048),
+        nn.ReLU(),
+        nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1),
+        nn.Dropout(p=0.5),
+        nn.Linear(in_features=2048, out_features=1)
+    )
+
+    return model
